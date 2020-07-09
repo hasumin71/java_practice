5cf7fdbc414dcc9f97d3d8dd03c50f822e3fad2e
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-09 11:40:33 +0900

    java_practice file changed

--- progate /Main.java
+++ progate /Main.java
@@ -1,8 +1,9 @@
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
       // 仕事を「医者」にしてください
       Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0, "医者");
-      person1.printData();
+    // メソッドの呼び出し
+      person1.printData();  
       // 仕事を「教師」にしてください
       Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0, "教師");
       person2.printData();
@@ -14,5 +15,4 @@ class Main {
       System.out.println("person1の仕事を"+ person1.getJob() +"に変更しました");
       person1.printData();
     }
-  }
-  
+}
