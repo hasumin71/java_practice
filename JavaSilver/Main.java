950932645155edbceb3a65366ec40ca9856fd798
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-21 15:37:31 +0900

    test 3_21

--- JavaSilver/Main.java
+++ JavaSilver/Main.java
@@ -1,8 +1,17 @@
-//3_9
+// //3_9
+// public class Main {
+//   public static void main(String[] args) {
+//     Sample a = new Sample(10, "a");
+//     Sample b = new Sample(10, "b");
+//     System.out.println(a.equals(b));
+//   }
+// }
+//3_21 yesと表示したい
 public class Main {
-  public static void main(String[] args) {
-    Sample a = new Sample(10, "a");
-    Sample b = new Sample(10, "b");
-    System.out.println(a.equals(b));
+  public static void main(String[] args){
+    String a = "A";
+    String b = "B";
+    String c = a.equals(b)? "no" : "yes"; //aとbは同値ではないのでが明らかなので、falseがyes出なければならない。
+    System.out.println(c);
   }
 }
