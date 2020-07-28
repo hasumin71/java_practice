2e9b812f283d0503cf8456bd2c5e3a34fb3ad1f8
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-28 14:23:53 +0900

    9_17 deleteメソッド

--- JavaSilver/Main.java
+++ JavaSilver/Main.java
@@ -429,10 +429,22 @@
 // }
 
 //9_16
-public class Main {
+// public class Main {
+//   public static void main(String[] args){
+//     StringBuilder sb = new StringBuilder("abc");
+//     sb.append("de").insert(2,"g");
+//     System.out.println(sb);
+//   }
+// }
+
+//9_17
+public class Main{
   public static void main(String[] args){
-    StringBuilder sb = new StringBuilder("abc");
-    sb.append("de").insert(2,"g");
+    StringBuilder sb = new StringBuilder();
+    sb.append("a");
+    sb.insert(1,"b");
+    sb.append("cde");
+    sb.delete(1,2);
     System.out.println(sb);
   }
 }
