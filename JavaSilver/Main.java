bd945e1a64138e88bdbdc91212445d3b45f2a356
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-07-31 09:51:58 +0900

    10_70 continueとラベル付きbreak

--- JavaSilver/Main.java
+++ JavaSilver/Main.java
@@ -669,15 +669,36 @@
 // }
 
 //10_64
+// public class Main {
+//   public static void main(String[] args){
+//     Sample[] array={
+//       new Sample(10),
+//       new Sample(20)
+//     };
+//     Sample[] array2 = new Sample[2];
+//     System.arraycopy(array, 0, array2, 0, array.length);
+//     array2[1].num = 10;
+//     System.out.println(array[1].num); //array[2]と同じインスタンスを持つので10になる
+//   }
+// }
+
+//10_70
 public class Main {
   public static void main(String[] args){
-    Sample[] array={
-      new Sample(10),
-      new Sample(20)
-    };
-    Sample[] array2 = new Sample[2];
-    System.arraycopy(array, 0, array2, 0, array.length);
-    array2[1].num = 10;
-    System.out.println(array[1].num); //array[2]と同じインスタンスを持つので10になる
+    int num = 1;
+    x:
+    for (int i = 0; i < 3; i++) {
+      y:
+      for(int j = 0; j < 5; j++){
+        if (j==1){ //変数jが０と２の時だけ変数numに加算されるがnumに加算するのはiだけなので
+          continue;
+        }
+        if(j==3){
+          break x; //breakが実行されたときには外側のループを抜ける
+        }
+        num += i;
+      }
+    }
+    System.out.println(num);
   }
 }
