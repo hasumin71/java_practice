1f3a29be41457aae17a5484eb381f0d601422f0a
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-08-03 00:46:15 +0900

    11_75 スーパークラスのコンストラクタの呼び出しについて

--- JavaSilver/A.java
+++ JavaSilver/A.java
@@ -1,5 +1,13 @@
-class A {
-  public A(){
-    System.out.println("A");
+// class A {
+//   public A(){
+//     System.out.println("A");
+//   }
+// }
+
+//11_75 //継承関係にあってもコンストラクタ は引き継げない
+public class A{
+  private int num;
+  public A(int num){
+    this.num = num;
   }
 }
--- JavaSilver/B.java
+++ JavaSilver/B.java
@@ -1,6 +1,14 @@
-class B extends A {
-  public B(){
-    //ここには表示されてないがコンパイルするとsuper();が追加されBクラスのインスタンス化によってこのコンストラクタの処理が行われるときsuperクラスのコンストラクタが呼び出される
-    System.out.println("B");
+// class B extends A {
+//   public B(){
+//     //ここには表示されてないがコンパイルするとsuper();が追加されBクラスのインスタンス化によってこのコンストラクタの処理が行われるときsuperクラスのコンストラクタが呼び出される
+//     System.out.println("B");
+//   }
+// }
+
+//11_75
+public class B extends A{
+  private String val;
+  public B(String val, int num){
+    super(num); //サブクラスにスーパークラスのコンストラクタの呼び出しが必要
   }
 }
