67a943f21f6d8cda8c61cff1b83698a68564527d
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-08-12 19:39:11 +0900

    M-SOLUTIONS プロコンオープン 2020 B 修正

--- AtCorder/Main.java
+++ AtCorder/Main.java
@@ -34,31 +34,31 @@
 // }
 
 //B
-import java.util.Scanner;
+// import java.util.Scanner;
  
-public class Main {
-  public static void main(String[] args) throws Exception {
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt(), 
-    b = sc.nextInt(), 
-    c = sc.nextInt(), 
-    k = sc.nextInt();
-    sc.close();
+// public class Main {
+//   public static void main(String[] args) throws Exception {
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt(), 
+//     b = sc.nextInt(), 
+//     c = sc.nextInt(), 
+//     k = sc.nextInt();
+//     sc.close();
 
-    for (;k > 0; k--) {
-      if (a >= b) {
-          b *= 2;
-      } else if (b >= c) {
-          c *= 2;
-      } else {
-          break;
-      }
-  }
+//     for (;k > 0; k--) {
+//       if (a >= b) {
+//           b *= 2;
+//       } else if (b >= c) {
+//           c *= 2;
+//       } else {
+//           break;
+//       }
+//   }
 
-  if (b > a && c > b) {
-      System.out.println("Yes");
-  } else {
-      System.out.println("No");
-    }
-  }
-}
+//   if (b > a && c > b) {
+//       System.out.println("Yes");
+//   } else {
+//       System.out.println("No");
+//     }
+//   }
+// }
